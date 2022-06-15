2715ceb2c767328fb57a3a350ed59c11a39a304b
edwin001-tech <edwinshitakwa8@gmail.com>
2022-06-15 10:08:51 +0300

    CORS for client server communication

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.use(bodyParser.json()); //parse json data
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE'); // tell clients methods allowed
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); //allow clients to set headers
+    next();
+});
+
 app.use('/feed', feedRoutes);
 
 
